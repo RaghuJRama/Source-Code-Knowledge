4352d202df3dbb55f05e49e07eca62c68c19669f
RaghuJRama <raghavendra.janakirama@gmail.com>
2019-09-05 14:44:24 +0530

    Create Common.cs

---
+++ C#/Common.cs
@@ -0,0 +1,133 @@
+/*
+        /// <summary>
+        /// Holds class name
+        /// </summary>
+        private const string ClassName = "Common";
+
+
+
+            // Method signature
+            const string MethodSignature = "";
+            // Arguments message
+            string ArgumentsMessage = string.Format("Args (: {0})", );
+
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Error message
+                string strErrorMessage = "";
+
+                // Logs the exception message
+                Log.LogExceptionToFile(MethodSignature, ClassName, this.ToString(), ArgumentsMessage, strErrorMessage, ex);
+
+		        throw ex;
+            }
+
+
+
+
+
+            // Method signature
+            const string MethodSignature = "";
+            // Arguments message
+            string ArgumentsMessage = string.Format("Args (: {0})", );
+
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Error message
+                string strErrorMessage = "";
+
+                // Logs the exception message
+                Log.LogExceptionToFile(MethodSignature, ClassName, ArgumentsMessage, strErrorMessage, ex);
+
+		        throw ex;
+            }
+
+
+
+
+
+            // Method signature
+            const string MethodSignature = "";
+
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Error message
+                string strErrorMessage = "";
+
+                // Logs the exception message
+                Log.LogExceptionToFile(MethodSignature, ClassName, this.ToString(), strErrorMessage, ex);
+
+		        throw ex;
+            }
+
+
+
+
+
+            // Method signature
+            const string MethodSignature = "";
+
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Error message
+                string strErrorMessage = "";
+
+                // Logs the exception message
+                Log.LogExceptionToFile(MethodSignature, ClassName, strErrorMessage, ex);
+
+		        throw ex;
+            }
+
+
+
+
+
+            // Method signature
+            const string MethodSignature = "";
+
+	        // Arguments message
+            StringBuilder sbArgs = new StringBuilder();
+
+            sbArgs.AppendFormat("Args (: {0}, ", );
+            sbArgs.AppendFormat(": {0}, ", );
+            sbArgs.AppendFormat(": {0})", );
+
+            string ArgumentsMessage = sbArgs.ToString();
+
+
+            try
+            {
+            }
+            catch (Exception ex)
+            {
+                // Error message
+                string strErrorMessage = "";
+
+                // Logs the exception message
+                Log.LogExceptionToFile(MethodSignature, ClassName, this.ToString(), ArgumentsMessage, strErrorMessage, ex);
+
+		        throw ex;
+            }
+
+
+
+
+
+
+ */
